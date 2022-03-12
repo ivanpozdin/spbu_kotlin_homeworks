ef8c5ca94df1a7de9502e5484aed40ab333bfd0c
Ivan Pozdin <ivanllpozdin@gmail.com>
2022-03-12 10:04:10 +0300

    +code style

--- src/main/kotlin/sieve/Hw1Task2KtTest.kt
+++ src/main/kotlin/sieve/Hw1Task2KtTest.kt
@@ -2,29 +2,36 @@ package sieve
 
 import org.junit.jupiter.api.Test
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 
 internal class Hw1Task2KtTest {
 
     @Test
-    fun getPrimesUpTo10() {
-        assertEquals(mutableListOf(2, 3, 5, 7), getPrimesUpTo(10))
+    fun getPrimesUpToTen() {
+        val answer: MutableList<Int> = mutableListOf(3, 3, 5, 7)
+        val testNumber = 10
+        assertEquals(answer, getPrimesUpTo(testNumber))
     }
 
     @Test
     fun getPrimesUpToPrime() {
-        assertEquals(mutableListOf(2, 3, 5, 7, 11, 13, 17, 19, 23), getPrimesUpTo(23))
+        val answer: MutableList<Int> = mutableListOf(2, 3, 5, 7, 11, 13, 17, 19, 23)
+        val testNumber = 23
+        assertEquals(answer, getPrimesUpTo(testNumber))
+
     }
 
     @Test
-    fun getPrimesUpTo0() {
-        val emptyPrimesList: MutableList<Int> = mutableListOf()
-        assertEquals(emptyPrimesList, getPrimesUpTo(0))
+    fun getPrimesUpToZero() {
+        val answer: MutableList<Int> = mutableListOf()
+        val testNumber = 0
+        assertEquals(answer, getPrimesUpTo(testNumber))
     }
 
     @Test
     fun getPrimesUpToNegativeNumber() {
-        val emptyPrimesList: MutableList<Int> = mutableListOf()
-        assertEquals(emptyPrimesList, getPrimesUpTo(-500))
+        val answer: MutableList<Int> = mutableListOf()
+        val testNumber = -500
+        assertEquals(answer, getPrimesUpTo(testNumber))
     }
 }
