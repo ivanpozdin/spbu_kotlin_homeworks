aa76937a4a927bf14ac906b979e12d17befaa054
Ivan Pozdin <ivanllpozdin@gmail.com>
2022-03-12 09:48:40 +0300

    +tests +function edit

--- src/main/kotlin/sieve/Hw1Task2KtTest.kt
+++ src/main/kotlin/sieve/Hw1Task2KtTest.kt
@@ -8,20 +8,23 @@ internal class Hw1Task2KtTest {
 
     @Test
     fun getPrimesUpTo10() {
-        assertEquals(mutableListOf(2,3,5,7), getPrimesUpTo(10))
+        assertEquals(mutableListOf(2, 3, 5, 7), getPrimesUpTo(10))
     }
+
     @Test
     fun getPrimesUpToPrime() {
-        assertEquals(mutableListOf(2,3,5,7,11,13,17,19,23), getPrimesUpTo(23))
+        assertEquals(mutableListOf(2, 3, 5, 7, 11, 13, 17, 19, 23), getPrimesUpTo(23))
     }
+
     @Test
     fun getPrimesUpTo0() {
-        val emptyPrimesList : MutableList<Int> = mutableListOf()
+        val emptyPrimesList: MutableList<Int> = mutableListOf()
         assertEquals(emptyPrimesList, getPrimesUpTo(0))
     }
+
     @Test
     fun getPrimesUpToNegativeNumber() {
-        val emptyPrimesList : MutableList<Int> = mutableListOf()
+        val emptyPrimesList: MutableList<Int> = mutableListOf()
         assertEquals(emptyPrimesList, getPrimesUpTo(-500))
     }
 }
