9c9236df6135be54d408c4f970fe36f27a931373
Ivan Pozdin <ivanllpozdin@gmail.com>
2022-04-03 08:45:34 +0300

    +изменил вопросы к пользователю

--- src/main/kotlin/homeworks/homework1/task3/PerformedCommandStorage.kt
+++ src/main/kotlin/homeworks/homework1/task3/PerformedCommandStorage.kt
@@ -2,36 +2,34 @@ package homeworks.homework1.task3
 
 class PerformedCommandStorage {
     var listOfNumbers: MutableList<Int> = mutableListOf()
-    private var previousListOfNumbers: MutableList<Int> = mutableListOf()
+    var previousListOfNumbers: MutableList<Int> = mutableListOf()
 
-    fun insetElementInTheBeginningOfList(x: Int){
-        previousListOfNumbers = listOfNumbers
+    fun insetElementInTheBeginningOfList(x: Int) {
+        previousListOfNumbers = listOfNumbers.toMutableList()
         listOfNumbers.add(0, x)
     }
-    fun insetElementInTheEndOfList(x: Int){
-        previousListOfNumbers = listOfNumbers
-        listOfNumbers.add(x)
 
+    fun insetElementInTheEndOfList(x: Int) {
+        previousListOfNumbers = listOfNumbers.toMutableList()
+        listOfNumbers.add(x)
     }
-    fun moveAnElement(i: Int, j: Int){
-        if ((i < 0) or (j < 0) or (i > listOfNumbers.size) or (j > listOfNumbers.size)){
+
+    fun moveAnElement(i: Int, j: Int) {
+        if ((i < 0) or (j < 0) or (i > listOfNumbers.size) or (j > listOfNumbers.size)) {
             println("Wrongs indexes")
             return
         }
-        previousListOfNumbers = listOfNumbers
+        previousListOfNumbers = listOfNumbers.toMutableList()
         val temp = listOfNumbers[i]
         listOfNumbers[i] = listOfNumbers[j]
         listOfNumbers[j] = temp
-
     }
-    fun cancelLastAction(){
-        if (previousListOfNumbers == mutableListOf<Int>()){
-            println("There was not last action")
-            return
-        }
-        val tempList: MutableList<Int> = listOfNumbers
-        listOfNumbers = previousListOfNumbers
-        previousListOfNumbers = tempList
+
+    fun cancelLastAction() {
+        require(previousListOfNumbers != emptyList<Int>()) { ("There was not last action") }
+        val tempList: MutableList<Int> = listOfNumbers.toMutableList()
+        listOfNumbers = previousListOfNumbers.toMutableList()
+        previousListOfNumbers = tempList.toMutableList()
     }
 
 }
--- src/main/kotlin/homeworks/homework1/task3/main.kt
+++ src/main/kotlin/homeworks/homework1/task3/main.kt
@@ -2,56 +2,40 @@ package homeworks.homework1.task3
 
 fun main() {
     println(
-        "Есть 4 комманды:" +
-            "\n1: вставить элемент x в начало списка, \n2: вставить элемент x в конец списка, \n3: переместить элемент" +
+        "Есть 5 комманд:" +
+            "\n0: остановить ввод" + "\n1: вставить элемент x в начало списка, " +
+            "\n2: вставить элемент x в конец списка, \n3: переместить элемент" +
             " с i на j позицию, \n4: отменить последнее действие"
     )
-    println(
-        "Для того, чтобы активировать соответсвующую комманду нужно, сначала ввести номер команды, а затем" +
-            " соотвествующие аргументы"
-    )
-
-    println("Для того, чтобы завершить ввод, введите \"0\"")
-    println("Первая комманда:\"1\" (вместо x нужно подставить ваше число)")
-    println("Вторая комманда:\"2\" (вместо x нужно подставить ваше число)")
-    println("Третья комманда:\"3\" (вместо i и j нужно подставить ваши индексы)")
-    println("Четвёртая комманда:\"4\"")
 
     val performedCommandStorage = PerformedCommandStorage()
     var command = 5
     while (command != 0) {
-        command = readLine()?.toInt() ?: 0
+        print("Введите номер комманды: ")
+        command = readLine()?.toIntOrNull() ?: 0
         when (command) {
             1 -> {
                 print("Введите число, которое нужно добавить в начало списка: ")
-                val x: Int = readLine()?.toInt() ?: 0
+                val x: Int = readLine()?.toIntOrNull() ?: 0
                 performedCommandStorage.insetElementInTheBeginningOfList(x)
-                for (i in 0 until performedCommandStorage.listOfNumbers.size)
-                    print("{performedCommandStorage.listOfNumbers[i]} ")
-                println()
+                print(performedCommandStorage.listOfNumbers.joinToString(", ", "", "\n"))
             }
             2 -> {
                 print("Введите число, которое нужно добавить в конец списка: ")
-                val x: Int = readLine()?.toInt() ?: 0
+                val x: Int = readLine()?.toIntOrNull() ?: 0
                 performedCommandStorage.insetElementInTheEndOfList(x)
-                for (i in 0 until performedCommandStorage.listOfNumbers.size)
-                    print("performedCommandStorage.listOfNumbers[i] ")
-                println()
+                print(performedCommandStorage.listOfNumbers.joinToString(", ", "", "\n"))
             }
             3 -> {
-                print("Введите 2 индекса( перемещение числа с 1ой позиции на 2ю: ")
-                val i: Int = readLine()?.toInt() ?: 0
-                val j: Int = readLine()?.toInt() ?: 0
+                print("Введите 2 индекса (перемещение числа с 1ой позиции на 2ю: ")
+                val i: Int = readLine()?.toIntOrNull() ?: 0
+                val j: Int = readLine()?.toIntOrNull() ?: 0
                 performedCommandStorage.moveAnElement(i, j)
-                for (t in 0 until performedCommandStorage.listOfNumbers.size)
-                    print("{performedCommandStorage.listOfNumbers[t]} ")
-                println()
+                print(performedCommandStorage.listOfNumbers.joinToString(", ", "", "\n"))
             }
             4 -> {
                 performedCommandStorage.cancelLastAction()
-                for (i in 0 until performedCommandStorage.listOfNumbers.size)
-                    print("{performedCommandStorage.listOfNumbers[i]} ")
-                println()
+                print(performedCommandStorage.listOfNumbers.joinToString(", ", "", "\n"))
             }
 
             0 -> {
@@ -62,7 +46,6 @@ fun main() {
             }
         }
     }
-
 }
 
 
