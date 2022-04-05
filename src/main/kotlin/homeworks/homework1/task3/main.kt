89c0f3c6787f76b0d665cd275cbee21978af05fd
Ivan Pozdin <ivanllpozdin@gmail.com>
2022-04-05 23:39:13 +0300

    +divided into functions

--- src/main/kotlin/homeworks/homework1/task3/main.kt
+++ src/main/kotlin/homeworks/homework1/task3/main.kt
@@ -4,14 +4,76 @@ const val QUIT_PROGRAM = 0
 const val CANCEL_LAST_ACTION = 4
 const val NOT_DEFINED_COMMAND = 5
 
-fun main() {
+fun insertInBeginning(performedCommandStorage: PerformedCommandStorage) {
+    var x: Int? = null
+    while (x == null) {
+        print("Введите число, которое нужно добавить в конец списка: ")
+        x = readLine()?.toIntOrNull() ?: run {
+            println("   Неверный аргумент, попробуйте ещё раз")
+            null
+        }
+    }
+    performedCommandStorage.insertInEnd(x)
+    print(performedCommandStorage.getListOfNumbers().joinToString(", ", "[", "]\n"))
+}
+
+fun insertInEnd(performedCommandStorage: PerformedCommandStorage) {
+    var x: Int? = null
+    while (x == null) {
+        print("Введите число, которое нужно добавить в конец списка: ")
+        x = readLine()?.toIntOrNull() ?: run {
+            println("   Неверный аргумент, попробуйте ещё раз")
+            null
+        }
+    }
+    performedCommandStorage.insertInEnd(x)
+    print(performedCommandStorage.getListOfNumbers().joinToString(", ", "[", "]\n"))
+}
+
+fun moveFromTo(performedCommandStorage: PerformedCommandStorage) {
+    var i: Int? = null
+    while (i == null) {
+        print("Введите индекс элемента, который нужно переместить: ")
+        i = readLine()?.toIntOrNull() ?: run {
+            println("   Неверный аргумент, попробуйте ещё раз")
+            null
+        }
+    }
+    var j: Int? = null
+    while (j == null) {
+        print("Введите индекс на который нужно переместить: ")
+        j = readLine()?.toIntOrNull() ?: run {
+            println("   Неверный аргумент, попробуйте ещё раз")
+            null
+        }
+    }
+    try {
+        performedCommandStorage.moveFromTo(i, j)
+    } catch (e: IllegalArgumentException) {
+        println(e.message)
+    }
+    print(performedCommandStorage.getListOfNumbers().joinToString(", ", "[", "]\n"))
+}
+
+fun cancelLastAction(performedCommandStorage: PerformedCommandStorage) {
+    try {
+        performedCommandStorage.cancelLastAction()
+    } catch (e: IllegalArgumentException) {
+        println(e.message)
+    }
+    print(performedCommandStorage.getListOfNumbers().joinToString(", ", "[", "]\n"))
+}
+
+fun printRules() {
     println("Есть 5 комманд:")
     println("0: остановить ввод")
     println("1: вставить элемент x в начало списка,")
     println("2: вставить элемент x в конец списка,")
     println("3: переместить элемент с i на j позицию,")
     println("4: отменить последнее действие")
+}
 
+fun processCommands() {
     val performedCommandStorage = PerformedCommandStorage()
     var command = NOT_DEFINED_COMMAND
     while (command != QUIT_PROGRAM) {
@@ -19,61 +81,16 @@ fun main() {
         command = readLine()?.toIntOrNull() ?: -1
         when (command) {
             INSERTION_IN_BEGINNING -> {
-                var x: Int? = null
-                while (x == null) {
-                    print("Введите число, которое нужно добавить в начало списка: ")
-                    x = readLine()?.toIntOrNull() ?: run {
-                        println("   Неверный аргумент, попробуйте ещё раз")
-                        null
-                    }
-                }
-                performedCommandStorage.insertInBeginning(x)
-                print(performedCommandStorage.getListOfNumbers().joinToString(", ", "[", "]\n"))
+                insertInBeginning(performedCommandStorage)
             }
             INSERTION_IN_END -> {
-
-                var x: Int? = null
-                while (x == null) {
-                    print("Введите число, которое нужно добавить в конец списка: ")
-                    x = readLine()?.toIntOrNull() ?: run {
-                        println("   Неверный аргумент, попробуйте ещё раз")
-                        null
-                    }
-                }
-                performedCommandStorage.insertInEnd(x)
-                print(performedCommandStorage.getListOfNumbers().joinToString(", ", "[", "]\n"))
+                insertInEnd(performedCommandStorage)
             }
             MOVING_FROM_TO -> {
-                var i: Int? = null
-                while (i == null) {
-                    print("Введите индекс элемента, который нужно переместить: ")
-                    i = readLine()?.toIntOrNull() ?: run {
-                        println("   Неверный аргумент, попробуйте ещё раз")
-                        null
-                    }
-                }
-                var j: Int? = null
-                while (j == null) {
-                    print("Введите индекс на который нужно переместить: ")
-                    j = readLine()?.toIntOrNull() ?: run {
-                        println("   Неверный аргумент, попробуйте ещё раз")
-                        null
-                    }
-                }
-                try {
-                    performedCommandStorage.moveFromTo(i, j)
-                } catch (e: IllegalArgumentException) {
-                    println(e.message)
-                }
-                print(performedCommandStorage.getListOfNumbers().joinToString(", ", "[", "]\n"))
+                moveFromTo(performedCommandStorage)
             }
             CANCEL_LAST_ACTION -> {
-                try {
-                    performedCommandStorage.cancelLastAction()
-                } catch (e: IllegalArgumentException) {
-                    println(e.message)
-                }
-                print(performedCommandStorage.getListOfNumbers().joinToString(", ", "[", "]\n"))
+                cancelLastAction(performedCommandStorage)
             }
             QUIT_PROGRAM -> {
                 println("Завершение работы")
@@ -83,4 +100,9 @@ fun main() {
             }
         }
     }
+}
+
+fun main() {
+    printRules()
+    processCommands()
 }
