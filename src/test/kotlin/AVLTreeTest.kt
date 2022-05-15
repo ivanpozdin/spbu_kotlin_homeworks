c22f30bff924de52da8449ae159fd9225c0a0801
Ivan Pozdin <ivanllpozdin@gmail.com>
2022-05-15 15:12:54 +0300

    Начал делать тесты

--- src/test/kotlin/AVLTreeTest.kt
+++ src/test/kotlin/AVLTreeTest.kt
@@ -1,5 +1,24 @@
-import org.junit.jupiter.api.Assertions.*
+import homeworks.homework3.task1.AVLTree
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
-internal class AVLTreeTest{
-
+internal class AVLTreeTest {
+    @Test
+    fun insertionTest() {
+        val expected = mutableSetOf<Int>()
+        val real = mutableSetOf<Int>()
+        val treeMap = AVLTree<Int, Int>(0, mutableSetOf(), mutableSetOf(), mutableListOf())
+        for (i in 1..1024) {
+            expected.add(i * 10)
+        }
+        for (i in 1..1024) {
+            treeMap[i] = i * 10
+        }
+        for (i in -2000..2000) {
+            if (treeMap.containsKey(i)) {
+                real.add(i * 10)
+            }
+        }
+        assertEquals(expected, real)
+    }
 }
