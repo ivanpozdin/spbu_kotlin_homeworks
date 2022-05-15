53806e53e496037bc10a1ad11282cd1fb248bba4
Ivan Pozdin <ivanllpozdin@gmail.com>
2022-05-15 14:12:02 +0300

    Небольшие изменения для codeStyle. В основном уменьшение кол-ва return'ов в ф-циях

--- src/main/kotlin/homeworks/homework3/task1/TreeNode.kt
+++ src/main/kotlin/homeworks/homework3/task1/TreeNode.kt
@@ -1,6 +1,6 @@
 package homeworks.homework3.task1
 
-class TreeNode<K : Comparable<K>, V>(var key: K, var value: V) {
+class TreeNode<K : Comparable<K>, V>(private var key: K, var value: V) {
     var leftChild: TreeNode<K, V>? = null
     var rightChild: TreeNode<K, V>? = null
     private var height: Int = 1
@@ -35,29 +35,29 @@ class TreeNode<K : Comparable<K>, V>(var key: K, var value: V) {
 
     private fun balance(): TreeNode<K, V>? {
         updateHeight()
+        val nodeToReturn: TreeNode<K, V>?
         if (getBalanceFactor() == 2) {
             if (rightChild?.getBalanceFactor() == -1) {
                 rightChild = rightChild?.rotateRight()
             }
-            return rotateLeft()
-        }
-        if (getBalanceFactor() == -2) {
+            nodeToReturn = rotateLeft()
+        } else if (getBalanceFactor() == -2) {
             if (leftChild?.getBalanceFactor() == 1) leftChild = leftChild?.rotateLeft()
-            return rotateRight()
+            nodeToReturn = rotateRight()
+        } else {
+            nodeToReturn = this
         }
-        return this
+        return nodeToReturn
     }
 
     fun findNodeWithGivenKey(givenKey: K, rootNode: TreeNode<K, V>? = this): TreeNode<K, V>? {
-        if (rootNode == null) {
-            return null
+        var nodeToReturn: TreeNode<K, V>? = rootNode
+        if (rootNode != null && givenKey > rootNode.key) {
+            nodeToReturn = findNodeWithGivenKey(givenKey, rootNode.rightChild)
+        } else if (rootNode != null && givenKey < rootNode.key) {
+            nodeToReturn = findNodeWithGivenKey(givenKey, rootNode.leftChild)
         }
-        if (givenKey > rootNode.key) {
-            return findNodeWithGivenKey(givenKey, rootNode.rightChild)
-        } else if (givenKey < rootNode.key) {
-            return findNodeWithGivenKey(givenKey, rootNode.leftChild)
-        }
-        return rootNode
+        return nodeToReturn
     }
 
     fun insert(
@@ -76,43 +76,40 @@ class TreeNode<K : Comparable<K>, V>(var key: K, var value: V) {
     }
 
     private fun extractMax(node: TreeNode<K, V>? = this): Pair<TreeNode<K, V>?, TreeNode<K, V>?> {
-        if (node == null) {
-            return Pair(null, null)
-        }
-        if (node.rightChild != null) {
+        val pairToReturn: Pair<TreeNode<K, V>?, TreeNode<K, V>?>
+        if (node?.rightChild != null) {
             val pair: Pair<TreeNode<K, V>?, TreeNode<K, V>?> = extractMax(node.rightChild)
             node.rightChild = pair.second
-            return Pair<TreeNode<K, V>?, TreeNode<K, V>?>(pair.first, node)
+            pairToReturn = Pair<TreeNode<K, V>?, TreeNode<K, V>?>(pair.first, node)
+        } else {
+            pairToReturn = Pair(node, node?.leftChild)
         }
-        return Pair<TreeNode<K, V>?, TreeNode<K, V>?>(node, node.leftChild)
+        return pairToReturn
     }
 
     fun deleteNode(
-        key: K?,
-        specialArgumentForOldValue: ValueOrNull<V>,
-        rootNode: TreeNode<K, V>? = this
+        key: K?, specialArgumentForOldValue: ValueOrNull<V>, rootNode: TreeNode<K, V>? = this
     ): TreeNode<K, V>? {
-        if (rootNode == null || key == null) return null
-        if (key < rootNode.key) {
-            rootNode.leftChild = deleteNode(key, specialArgumentForOldValue, rootNode.leftChild)
-        } else if (key > rootNode.key) {
-            rootNode.rightChild = deleteNode(key, specialArgumentForOldValue, rootNode.rightChild)
+        var newRoot: TreeNode<K, V>? = rootNode
+        if (newRoot == null || key == null) {
+            newRoot = null
+        } else if (key < newRoot.key) {
+            newRoot.leftChild = deleteNode(key, specialArgumentForOldValue, newRoot.leftChild)
+        } else if (key > newRoot.key) {
+            newRoot.rightChild = deleteNode(key, specialArgumentForOldValue, newRoot.rightChild)
         } else {
-            specialArgumentForOldValue.value = rootNode.value
-
-            val newRoot: TreeNode<K, V>?
-            if (rootNode.leftChild == null) {
-                newRoot = rootNode.rightChild
-            } else if (rootNode.rightChild == null) {
-                newRoot = rootNode.leftChild
+            specialArgumentForOldValue.value = newRoot.value
+            if (newRoot.leftChild == null) {
+                newRoot = newRoot.rightChild
+            } else if (newRoot.rightChild == null) {
+                newRoot = newRoot.leftChild
             } else {
-                val pair: Pair<TreeNode<K, V>?, TreeNode<K, V>?>? = rootNode.leftChild?.extractMax()
+                val pair: Pair<TreeNode<K, V>?, TreeNode<K, V>?>? = newRoot.leftChild?.extractMax()
                 newRoot = pair?.first
                 newRoot?.leftChild = pair?.second
-                newRoot?.rightChild = rootNode.rightChild
+                newRoot?.rightChild = rootNode?.rightChild
             }
-            return newRoot?.balance()
         }
-        return rootNode.balance()
+        return newRoot?.balance()
     }
 }
--- src/main/kotlin/homeworks/homework3/task1/main.kt
+++ src/main/kotlin/homeworks/homework3/task1/main.kt
@@ -1,28 +1,2 @@
 package homeworks.homework3.task1
 
-fun main() {
-    val treeMap = AVLTree<Int, Int>(0, mutableSetOf(), mutableSetOf(), mutableListOf())
-
-    for (i in 1..1) {
-        treeMap[i] = i * 10
-    }
-
-    for (i in 1..1) {
-        print("${treeMap[i]}\n")
-    }
-    print("\n")
-    print("${treeMap.containsKey(1)}\n")
-    print("${treeMap.containsKey(2)}\n")
-    print("${treeMap.containsValue(10)}\n")
-    print("${treeMap.remove(1)}\n")
-    print("${treeMap.containsKey(1)}\n")
-    print("${treeMap.containsKey(2)}\n")
-    print("${treeMap.containsValue(10)}\n")
-    print("${treeMap[1]}\n")
-    print("${treeMap.isEmpty()}\n")
-
-    for (i in 1..1024) {
-        treeMap[i] = i * 10
-    }
-    print("${treeMap[100]}\n")
-}
