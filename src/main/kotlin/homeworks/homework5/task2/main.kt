a0644f5317002ed27347b839b65b9d0499dadea5
Ivan Pozdin <ivanllpozdin@gmail.com>
2022-09-24 17:03:59 +0300

    Small CLI changes.

--- src/main/kotlin/homeworks/homework5/task2/main.kt
+++ src/main/kotlin/homeworks/homework5/task2/main.kt
@@ -1,12 +1,15 @@
 package homeworks.homework5.task2
 
-import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import kotlin.system.measureTimeMillis
 
 fun getSizeAmount(message: String): Int {
     var inputSize = ""
     while (inputSize.toIntOrNull() == null || inputSize.toIntOrNull()!! <= 0) {
-        print("Введите кол-во $message: ")
+        print("Введите $message: ")
         inputSize = readLine()!!
         if (inputSize.toIntOrNull() == null || inputSize.toIntOrNull()!! <= 0) {
             println("Внимание: вводить можно только натуральные числа. Попробуйте снова.")
@@ -15,35 +18,12 @@ fun getSizeAmount(message: String): Int {
     return inputSize.toInt()
 }
 
-fun getMatrix(rawAmount: Int, columnAmount: Int, message: String): Array<IntArray> {
-    require(rawAmount > 0 && columnAmount > 0) { "Количество стобцов и строк у матрицы должно быть положительным!" }
-    println(message)
-    val matrix = Array(rawAmount) { _ ->
-        var isInputNotOk = true
-        var raw = IntArray(columnAmount) { 0 }
-        while (isInputNotOk) {
-            try {
-                raw = readLine()!!.split(" ").map { it.toInt() }.toIntArray()
-                if (raw.size == columnAmount) {
-                    isInputNotOk = false
-                }
-            } catch (e: NumberFormatException) {
-                isInputNotOk = true
-                println("Что-то не так с этой строкой. Введите её снова.")
-            }
-        }
-        raw
-    }
-    return matrix
-}
-
 fun multiplyMatricesInOneThread(firstMatrix: Array<IntArray>, secondMatrix: Array<IntArray>): Array<IntArray> {
     val rawAmountOfFirstMatrix = firstMatrix.size
     val columnAmountOfFirstMatrix = firstMatrix[0].size
     val rawAmountOfSecondMatrix = secondMatrix.size
     val columnAmountOfSecondMatrix = secondMatrix[0].size
-    require(columnAmountOfFirstMatrix == rawAmountOfSecondMatrix)
-    {
+    require(columnAmountOfFirstMatrix == rawAmountOfSecondMatrix) {
         "Размеры матриц некорректны"
     }
     val resultMatrix = Array(rawAmountOfFirstMatrix) { IntArray(columnAmountOfSecondMatrix) }
@@ -63,8 +43,7 @@ fun multiplyMatricesWithCoroutines(firstMatrix: Array<IntArray>, secondMatrix: A
     val columnAmountOfFirstMatrix = firstMatrix[0].size
     val rawAmountOfSecondMatrix = secondMatrix.size
     val columnAmountOfSecondMatrix = secondMatrix[0].size
-    require(columnAmountOfFirstMatrix == rawAmountOfSecondMatrix)
-    {
+    require(columnAmountOfFirstMatrix == rawAmountOfSecondMatrix) {
         "Размеры матриц некорректны"
     }
     val resultMatrix = Array(rawAmountOfFirstMatrix) { IntArray(columnAmountOfSecondMatrix) }
@@ -89,66 +68,24 @@ fun multiplyMatricesWithCoroutines(firstMatrix: Array<IntArray>, secondMatrix: A
     return resultMatrix
 }
 
-fun generateMatrix(raws: Int, columns: Int): Array<IntArray> {
-    return Array<IntArray>(raws) {
-        IntArray(columns) {
-            kotlin.random.Random.nextInt(0, 3)
-        }
+fun generateMatrix(raws: Int, columns: Int): Array<IntArray> = Array<IntArray>(raws) {
+    IntArray(columns) {
+        kotlin.random.Random.nextInt(-5, 5)
     }
 }
+
 
 fun main() {
-    val m = 2000
+    println("Введите размерность квадратных матриц, которые будут случайно сгенерированы и перемножены с помощью корутин и без них:")
+    val m = getSizeAmount("размерность")
     val firstMatrix = generateMatrix(m, m)
     val secondMatrix = generateMatrix(m, m)
-    val time3 = measureTimeMillis {
-        val resultMatrix3 = multiplyMatricesWithCoroutines(firstMatrix, secondMatrix)
-        println("результат 3 ${resultMatrix3[0][0]}")
+    val timeWithCoroutines = measureTimeMillis {
+        val resultMatrixWithCoroutines = multiplyMatricesWithCoroutines(firstMatrix, secondMatrix)
     }
-    println(time3)
-    val time1 = measureTimeMillis {
-        val resultMatrix1 = multiplyMatricesInOneThread(firstMatrix, secondMatrix)
-        println("результат 1 ${resultMatrix1[0][0]}")
+    println("Время умножения матриц с помощью корутин: $timeWithCoroutines мс")
+    val timeWithOneThread = measureTimeMillis {
+        val resultMatrixWithOneThread = multiplyMatricesInOneThread(firstMatrix, secondMatrix)
     }
-    println(time1)
+    println("Время умножения матриц без корутин: $timeWithOneThread мс")
 }
-
-
-//    val rawAmountOfFirstMatrix: Int = getSizeAmount("строк в 1й матрице")
-//    val columnAmountOfFirstMatrix: Int = getSizeAmount("столбцов в 1й матрице")
-//
-//    val rawAmountOfSecondMatrix: Int = getSizeAmount("строк во 2й матрице")
-//    val columnAmountOfSecondMatrix: Int = getSizeAmount("столбцов во 2й матрице")
-//
-//    val firstMatrix = getMatrix(
-//        rawAmountOfFirstMatrix,
-//        columnAmountOfFirstMatrix,
-//        "Введите первую матрицу построчно, разделяя элементы пробелами."
-//    )
-//    val secondMatrix = getMatrix(
-//        rawAmountOfSecondMatrix,
-//        columnAmountOfSecondMatrix,
-//        "Введите вторую матрицу построчно, разделяя элементы пробелами."
-//    )
-
-
-//    for (i in resultMatrix) {
-//        for (j in i) {
-//            print("$j ")
-//        }
-//        println()
-//    }
-//}
-
-//for (i in firstMatrix.indices) {
-//    for (j in secondMatrix[0].indices) {
-//        resultMatrixAsync[i * j] = async() {
-//            var resultToReturn: Int = 0
-//            for (k in secondMatrix.indices) {
-//                resultToReturn += firstMatrix[i][k] * secondMatrix[k][j]
-//            }
-//            resultToReturn
-//        }
-//
-//    }
-//}
