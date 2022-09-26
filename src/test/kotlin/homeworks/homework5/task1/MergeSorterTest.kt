39e7b80c422938281e53229436615c049d16a7d6
Ivan Pozdin <ivanllpozdin@gmail.com>
2022-09-26 12:39:51 +0300

    feat: add parameterizedTest for mergeSortWithCoroutines() function

--- src/test/kotlin/homeworks/homework5/task1/MergeSorterTest.kt
+++ src/test/kotlin/homeworks/homework5/task1/MergeSorterTest.kt
@@ -1,20 +1,38 @@
 package homeworks.homework5.task1
 
 import org.junit.jupiter.api.Assertions.assertArrayEquals
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import kotlin.random.Random
 
 internal class MergeSorterTest {
+    companion object {
+        @JvmStatic
+        fun mergeSortTestInputData() = mutableListOf(Arguments.of(
+            IntArray(1000) { it + 1 }, IntArray(1000) { 1000 - it }, 0
+        ), Arguments.of(
+            IntArray(1000) { it + 1 }, IntArray(1000) { 1000 - it }, 2
+        ), Arguments.of(
+            IntArray(1000) { it + 1 }, IntArray(1000) { 1000 - it }, 4
+        ), Arguments.of(
+            IntArray(1000) { it + 1 }, IntArray(1000) { 1000 - it }, 15
+        ), Arguments.of(
+            IntArray(9999) { it + 1 }, IntArray(9999) { 9999 - it }, 15
+        )
+        ) + MutableList(15) {
+            val array = IntArray(Random.nextInt(1, 10000)) { Random.nextInt(-1000, 1000) }
+            val arrayForStdSort = array.copyOf()
+            val coroutinesAmount = Random.nextInt(0, 2048)
+            arrayForStdSort.sort()
+            Arguments.of(arrayForStdSort, array, coroutinesAmount)
+        }
+    }
 
-    @Test
-    fun mergeSortWithCoroutines() {
-        val size: Int = Random.nextInt(200)
-        val amountOfThreads: Int = Random.nextInt(100)
-        val list = IntArray(size) { Random.nextInt(2 * size - 1) - (size - 1) }
-        val listForStdSort = list.copyOf()
-        val listForMySort = list.copyOf()
-        listForStdSort.sort()
-        MergeSorter.mergeSortWithCoroutines(listForMySort, amountOfThreads)
-        assertArrayEquals(listForStdSort, listForMySort)
+    @ParameterizedTest(name = "case {index}")
+    @MethodSource("mergeSortTestInputData")
+    fun sortTest(expected: IntArray, array: IntArray, coroutinesAmount: Int) {
+        MergeSorter.mergeSortWithCoroutines(array, coroutinesAmount)
+        assertArrayEquals(expected, array)
     }
 }
